1ef3d7475f87817efb4b947d5b5f7c1206624ba1
PanWilson <PanWilsonn@wp.pl>
2018-04-28 00:06:41 +0200

    (skip)

---
+++ Assets/_FarmEscapeSimulator/_SCRIPTS/LOGIC/ScriptableObjects/CreateLauncherStats.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class CreateLauncherStats
+{
+    [MenuItem("Assets/Create/Launcher Stats")]
+    public static void CreateMyAsset()
+    {
+        LauncherStats asset = ScriptableObject.CreateInstance<LauncherStats>();
+
+        AssetDatabase.CreateAsset(asset, "Assets/NewLauncherStats.asset");
+        AssetDatabase.SaveAssets();
+
+        EditorUtility.FocusProjectWindow();
+
+        Selection.activeObject = asset;
+    }
+}
---
+++ Assets/_FarmEscapeSimulator/_SCRIPTS/LOGIC/ScriptableObjects/LauncherStatsClass.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[CreateAssetMenu(fileName = "Data", menuName = "Prefs", order = 1)]
+public class LauncherStatsClass : ScriptableObject
+{
+    public string objectName = "New LauncherStats";
+    public bool colorIsRandom = false;
+    public Color thisColor = Color.white;
+}
