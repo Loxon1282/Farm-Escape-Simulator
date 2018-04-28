f76c253062050b08743693433f03953dd9cbba7a
PanWilson <PanWilsonn@wp.pl>
2018-04-28 23:23:32 +0200

    Clicable animal

--- Assets/_FarmEscapeSimulator/_SCRIPTS/LOGIC/AnimalController.cs
+++ Assets/_FarmEscapeSimulator/_SCRIPTS/LOGIC/AnimalController.cs
@@ -13,11 +13,48 @@ public class AnimalController : MonoBehaviour {
         FocusCameraOnAnimal();
 
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.touchCount > 0)
+        {
+
+            foreach (Touch touch in Input.touches)
+            {
+                if (touch.phase == TouchPhase.Began)
+                {
+                    Ray ray = Camera.main.ScreenPointToRay(touch.position);
+                    RaycastHit[] hits;
+                    if ((hits = Physics.RaycastAll(ray)) != null)
+                    {
+                        foreach (RaycastHit h in hits)
+                        {
+                            if (h.collider.gameObject.tag == "projectile") animal.GetComponent<ProjectileComponent>().AnimalInteraction();
+                        }
+                    }
+                }
+            }
+        }
+            if (Input.GetMouseButtonDown(0))
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit[] hits;
+                if ((hits = Physics.RaycastAll(ray)) != null)
+                {
+                    foreach (RaycastHit h in hits)
+                    {
+                    if (h.collider.gameObject.tag == "projectile")
+                    {
+                        animal.GetComponent<ProjectileComponent>().AnimalInteraction();
+                    }
+                    }
+                }
+            }
+        
+    }
+
 
    public void SetAnimal(GameObject obj)
     {
--- Assets/_FarmEscapeSimulator/_SCRIPTS/LOGIC/ProjectileComponent.cs
+++ Assets/_FarmEscapeSimulator/_SCRIPTS/LOGIC/ProjectileComponent.cs
@@ -17,7 +17,44 @@ public class ProjectileComponent : MonoBehaviour {
 
     private void OnTriggerEnter(Collider other)
     {
-        
+        switch (other.tag)
+        {
+            case "baloon":
+                print(other.tag);
+                break;
+            case "bird":
+                print(other.tag);
+                break;
+            case "coin":
+                print(other.tag);
+                break;
+            case "feedBag":
+                print(other.tag);
+                break;
+            case "flyingMine":
+                print(other.tag);
+                break;
+            case "gears":
+                print(other.tag);
+                break;
+            case "mine":
+                print(other.tag);
+                break;
+            case "plank":
+                print(other.tag);
+                break;
+            case "screw":
+                print(other.tag);
+                break;
+            case "specialFeedBag":
+                print(other.tag);
+                break;
+        }
+    }
+
+    public void AnimalInteraction()
+    {
+        gameObject.transform.localScale = new Vector3(0.5f,0.5f,0.5f);
     }
 
 }
