4d64e280f18855bd05f3010078c20bb50f4ed0ac
Loxon1282 <loxon1282@gmail.com>
2018-04-28 15:55:13 +0200

    Commit obstacles pooling and scene tests

---
+++ Assets/_FarmEscapeSimulator/_SCRIPTS/GENERATORS/ObstacleGenerator.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleGenerator : MonoBehaviour {
+
+    [System.Serializable]
+    public class Obstacle
+    {
+
+        public string tag;
+        public GameObject prefab;
+        public int size;
+    }
+
+    #region Singleton
+    public static ObstacleGenerator Instance;
+    #endregion
+
+
+    public List<Obstacle> obstacles;
+    public Dictionary<string, Queue<GameObject>> obstaclePoolDictionary;
+
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        CrateObstaclesDictionary();
+    }
+    void Start () {
+
+       // CrateObstaclesDictionary();
+
+    }
+    public void CrateObstaclesDictionary()
+    {
+        obstaclePoolDictionary = new Dictionary<string, Queue<GameObject>>();
+
+        foreach (Obstacle ob in obstacles)
+        {
+            Queue<GameObject> obstacleObj = new Queue<GameObject>();
+
+            for (int i = 0; i < ob.size; i++)
+            {
+                GameObject obj = Instantiate(ob.prefab);
+                obj.SetActive(false);
+                obstacleObj.Enqueue(obj);
+
+            }
+
+            obstaclePoolDictionary.Add(ob.tag, obstacleObj);
+        }
+
+    }
+	
+    public GameObject SpawnObstacle(string tag,Vector3 position)
+    {
+        if (!obstaclePoolDictionary.ContainsKey(tag))
+        {
+            Debug.Log("DODAJ SE PULAPKE Z TYM TAGIEM CFELU");
+            return null;
+        }
+        GameObject objectToSpawn = obstaclePoolDictionary[tag].Dequeue();
+
+        objectToSpawn.SetActive(true);
+        objectToSpawn.transform.position = position;
+
+        obstaclePoolDictionary[tag].Enqueue(objectToSpawn);
+        return objectToSpawn;
+    }
+	void Update () {
+		
+	}
+}
---
+++ Assets/_FarmEscapeSimulator/_SCRIPTS/GENERATORS/ObstacleSpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleSpawner : MonoBehaviour {
+
+   ObstacleGenerator generator;
+
+    void Awake()
+    {
+        generator = ObstacleGenerator.Instance;
+    }
+    void Start () {
+
+
+
+    }
+    public IEnumerator lateSpawn()
+    {
+        for (int i = 0; i < 20; i++)
+        {
+            yield return new WaitForSeconds(.5f);
+            ObstacleGenerator.Instance.SpawnObstacle("a", new Vector3(i, 0, 0));
+
+        }
+    }
+
+
+}
