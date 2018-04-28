ed6df2ef5d5ab66c846e5231f454ec0a7aebeedc
Loxon1282 <loxon1282@gmail.com>
2018-04-28 03:31:02 +0200

    Some Shieeet

--- Assets/_FarmEscapeSimulator/_SCRIPTS/TOUCH_CONTROLERS/Swipe.cs
+++ Assets/_FarmEscapeSimulator/_SCRIPTS/TOUCH_CONTROLERS/Swipe.cs
@@ -8,36 +8,42 @@ public class Swipe : MonoBehaviour {
     private bool isDraging = false;
     private Vector2 startTouch, swipeDelta;
 
+    public Vector2 SwipeDelta { get { return swipeDelta; } }
+    public bool Tap { get { return tap; } }
+    public bool SwipeLeft { get { return swipeLeft; } }
+    public bool SwipeRight { get { return swipeRight; } }
+    public bool SwipeUp { get { return swipeUp; } }
+    public bool SwipeDown { get { return swipeDown; } }
 
+    private void Update()
+    {
 
-    void Update() {
         tap = swipeLeft = swipeRight = swipeUp = swipeDown = false;
 
         #region Standalone Inputs
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButtonDown(0))
         {
             tap = true;
             isDraging = true;
             startTouch = Input.mousePosition;
-
         }
-        else if (Input.GetMouseButtonDown(0))
+        else if (Input.GetMouseButtonUp(0))
         {
-            isDraging = true;
+            isDraging = false;
             Reset();
         }
         #endregion
 
-        #region Mobile Inputs
-        if (Input.touches.Length != 0)
+        #region Mobile Input
+        if (Input.touches.Length > 0)
         {
-            if(Input.touches[0].phase == TouchPhase.Began)
+            if (Input.touches[0].phase == TouchPhase.Began)
             {
-                tap = true;
                 isDraging = true;
+                tap = true;
                 startTouch = Input.touches[0].position;
             }
-            else if(Input.touches[0].phase== TouchPhase.Ended || Input.touches[0].phase == TouchPhase.Canceled)
+            else if (Input.touches[0].phase == TouchPhase.Ended || Input.touches[0].phase == TouchPhase.Canceled)
             {
                 isDraging = false;
                 Reset();
@@ -45,64 +51,45 @@ public class Swipe : MonoBehaviour {
         }
         #endregion
 
-        //Calculate the distance
+        // Calculate the distance
         swipeDelta = Vector2.zero;
-        if(isDraging)
+        if (isDraging)
         {
             if (Input.touches.Length > 0)
-            {
                 swipeDelta = Input.touches[0].position - startTouch;
-                
-            }
-            else if(Input.GetMouseButton(0))
-            {
+            else if (Input.GetMouseButton(0))
                 swipeDelta = (Vector2)Input.mousePosition - startTouch;
-            }
         }
-        if(swipeDelta.magnitude > 125.0f)
+
+        //Did we cross the distance?
+        if (swipeDelta.magnitude > 125)
         {
             //Which direction?
             float x = swipeDelta.x;
             float y = swipeDelta.y;
-            
-            if(Mathf.Abs(x)> Mathf.Abs(y))
+            if (Mathf.Abs(x) > Mathf.Abs(y))
             {
-                //Left or Right
+                //Left or right
                 if (x < 0)
-                {
                     swipeLeft = true;
-                }
                 else
-                {
                     swipeRight = true;
-                }
             }
             else
             {
-                //Up or down
-                if (x < 0)
+                // Up or down
+                if (y < 0)
                     swipeDown = true;
                 else
                     swipeUp = true;
             }
-
             Reset();
-
         }
-
     }
 
-    private void Reset()
+    void Reset()
     {
         startTouch = swipeDelta = Vector2.zero;
         isDraging = false;
     }
-
-    public bool Tap { get { return tap; } }
-    public Vector2 SwipeDelta { get { return swipeDelta; } }
-    public bool SwipeLeft { get { return swipeLeft; } }
-    public bool SwipeRight { get { return swipeRight; } }
-    public bool SwipeUp { get { return swipeUp; } }
-    public bool SwipeDown { get { return swipeDown; } }
-
 }
