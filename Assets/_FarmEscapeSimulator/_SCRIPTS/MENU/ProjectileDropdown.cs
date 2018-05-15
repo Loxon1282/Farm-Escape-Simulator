0214f69b52030320e34ddfc08c82cc34066fac0b
PanWilson <PanWilsonn@wp.pl>
2018-05-15 22:23:52 +0200

    proba ratowania ragdolla

---
+++ Assets/_FarmEscapeSimulator/_SCRIPTS/MENU/ProjectileDropdown.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileDropdown : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+        foreach(string n in GameManager.Instance.GetProjectileNames())
+        {
+            print(n);
+        }
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/_FarmEscapeSimulator/_SCRIPTS/MENU/SmallPanel.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmallPanel : MonoBehaviour {
+
+
+    [SerializeField]
+    GameObject Panel;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void TogglePanel()
+    {
+        if (Panel != null)
+        {
+            if (Panel.activeInHierarchy) DisablePanel();
+            else EnablePanel();
+        }
+        else Debug.LogError("No Panel assigned");
+    }
+
+    public void EnablePanel()
+    {
+        if (Panel != null) Panel.SetActive(true);
+        else Debug.LogError("No Panel assigned");
+    }
+
+    public void DisablePanel()
+    {
+        if (Panel != null) Panel.SetActive(false);
+        else Debug.LogError("No Panel assigned");
+    }
+
+}
